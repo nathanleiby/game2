d4d57e6c49ec5ff58d1a97a55bc3cdcee627f19e
Nathan Leiby <nathanleiby@gmail.com>
2023-10-21 14:09:25 -0700

    first pass on scoring / timing .. buggy
    https://gdquest.mavenseed.com/lessons/adding-scoring-and-timing

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Notes
 
 - bps and hbps aren't correct naming, i think. should be "beat **duration**" and half-beat duration. (music wise, quarter note and eighth note would make more sense, but I can see how we may be trying to avoid those names)
+- Godot 4
+  - tweens have changed
+  - event connecting has changed .. this syntax is closest to the old, and lets you pass a function as an argument (hooray!) `Events.connect("beat_incremented", _spawn_beat)` ([reference](https://gdquest.mavenseed.com/community/17073-notes-for-godot-4))
--- RhythmGame/Hits/HitBeat.gd
+++ RhythmGame/Hits/HitBeat.gd
@@ -3,11 +3,31 @@ extends Node2D
 
 var _beat_hit := false
 
-@onready var _animation_player := $AnimationPlayer
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
 @onready var _sprite := $Sprite2D
 @onready var _touch_area := $Area2D
 @onready var _label := $LabelCustom
 	
+# Scoring
+const SCORE_PERFECT := 10
+const SCORE_GREAT := 5
+const SCORE_OK := 3
+
+const OFFSET_PERFECT := 4
+const OFFSET_GREAT := 8
+const OFFSET_OK := 16
+
+const RADIUS_START := 150.0
+const RADIUS_PERFECT := 70.0
+
+# duration in beats for target circle to shrink
+const BEAT_DELAY := 4.0
+
+# radius of animated ring around note
+var _radius := RADIUS_START
+# speed at which target circle shrinks in pixels per second
+var _speed  := 0.0
+
 func _ready():
 	_animation_player.play("show")
 
@@ -15,19 +35,46 @@ func setup(data: Dictionary):
 	self.order_number = data.half_beat
 	global_position = data.global_position
 	_sprite.frame = data.color
+	_speed = 1.0 / data.beat_duration / BEAT_DELAY
+
+func _process(delta: float):
+	if _beat_hit:
+		return
+		
+	_radius -= delta * (RADIUS_START - RADIUS_PERFECT) * _speed
 	
-
-
-func _on_area_2d_input_event(viewport: Viewport, event: InputEvent, shape_idx: int):
-	if event.is_action_pressed("touch"):
-		_beat_hit = true
+	# The note has expired
+	if _radius <= RADIUS_PERFECT - OFFSET_PERFECT:
 		# disable further collisions by removing physics layer
 		_touch_area.collision_layer = 0
+		_beat_hit = true 
+		
+		Events.emit_signal("scored", {"score": 0, "position": global_position})
+		_animation_player.play("destroy")
+		
+		
+func _on_area_2d_input_event(viewport: Viewport, event: InputEvent, shape_idx: int):
+	if event.is_action_pressed("touch"):
+		# disable further collisions by removing physics layer
+		_touch_area.collision_layer = 0
+		_beat_hit = true
+		
+		Events.emit_signal("scored", {"score": _get_score(), "position": global_position})
 		# run destroy animation (which calls queue_free() at the end)
 		_animation_player.play("destroy")
 		
 
-
+func _get_score() -> int:
+	var offset = abs(RADIUS_PERFECT - _radius)
+	if offset < OFFSET_PERFECT:
+		return SCORE_PERFECT
+	elif offset < OFFSET_GREAT:
+		return SCORE_GREAT
+	elif offset < OFFSET_OK:
+		return SCORE_OK
+	else:
+		return 0
+		
 # TODO: setget syntax converted Godot 3 -> 4 .. works?
 @export var order_number: int = 0:
 	get:
