5f3b816e1042a893a96a1d6cc5e11e24f6e71267
Nathan Leiby <nathanleiby@gmail.com>
2023-10-22 23:32:21 -0700

    ui polish -- change tile sizes based on screen position

--- RhythmGame/UI/TrackSelector/track_tiles.gd
+++ RhythmGame/UI/TrackSelector/track_tiles.gd
@@ -13,8 +13,9 @@ var WIGGLE := 0 # _starting_x_offset = 0.0
 var _tween: Tween
 
 func _ready():
-	WIGGLE = position.x
+	WIGGLE = position.x # TODO: can remove?
 	_generate_tiles()
+	_update_tile_visuals()
 	
 	
 var _min_x_position := 0
@@ -46,6 +47,11 @@ func scroll(amount: Vector2):
 	position.x = clamp(position.x + amount.x, _min_x_position, _max_x_position)
 	if _tween != null:
 		_tween.stop()
+	_update_tile_visuals()
+	
+func _process(_delta: float):
+	if _tween != null and _tween.is_running():
+		_update_tile_visuals()
 	
 func _on_drag_detector_dragged(amount):
 	scroll(amount)
@@ -53,13 +59,13 @@ func _on_drag_detector_dragged(amount):
 func _snap_to_track(track_tile: TrackTile):
 	var offset_to_center := Vector2(get_parent().global_position.x - track_tile.global_position.x, 0)
 	
-	var _tween := create_tween()
+	_tween = create_tween()
 	_tween.set_ease(Tween.EASE_OUT)
 	_tween.set_trans(Tween.TRANS_EXPO)
 	_tween.tween_property(self, "position", position + offset_to_center, 0.5).from_current()
 	
+	
 func _on_select_area_track_selected(track_tile: TrackTile):
-	print("_on_select_area_track_selected", track_tile.track_data.label)
 	_selected_track_tile = track_tile
 
 func _unhandled_input(event: InputEvent):
@@ -68,3 +74,14 @@ func _unhandled_input(event: InputEvent):
 
 func _on_align_timer_timeout():
 	_snap_to_track(_selected_track_tile)
+
+func _update_tile_visuals():
+	var carousel_position_x: float = get_parent().global_position.x
+	for track_tile in _track_tiles:
+		var distance_to_view_center: float = abs(track_tile.global_position.x - carousel_position_x)
+		
+		var distance_normalized = remap(distance_to_view_center, 0.0, carousel_position_x, 0.0, 1.0)
+		
+		track_tile.scale = Vector2.ONE * (1.0 - distance_normalized)
+		track_tile.modulate.a = (1.0 - pow(distance_normalized, 3.0)) 
+		
